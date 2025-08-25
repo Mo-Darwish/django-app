978e2131fb1df7f3bd1547974efb25cbc39d5a3e
Mohammed Darwish <muhadar.univ@gmail.com>
2025-08-25 12:33:54 +0300

    feat(auth): add password reset functionality

--- demoapp/accounts/forms.py
+++ demoapp/accounts/forms.py
@@ -17,3 +17,19 @@ class RegisterForm(forms.ModelForm):
         if password and password_confirm and password != password_confirm:
             raise forms.ValidationError("Passwords do not match!")
         return cleaned_data
+
+class ResetPasswordForm(forms.ModelForm) :
+    email = forms.EmailField(widget= forms.EmailInput)
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+    class Meta:
+        model = User
+        fields  = ['email', 'password', 'password_confirm']
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+        # Check if the passwords match
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Passwords do not match!")
+        return cleaned_data
